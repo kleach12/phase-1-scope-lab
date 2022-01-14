bc221ab0c1390751bdcf06fd31fdb754cc7da072
Kale Leach <kleach1212@gmail.com>
2022-01-13 18:25:08 -0800

    commit tree 647fefe8051ff26d84d9a1c45348fbfbd7b77a4f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'Bobby'
+function upperCaseCustomerName() {
+     return customerName = 'BOB';
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'Bob'
+}
